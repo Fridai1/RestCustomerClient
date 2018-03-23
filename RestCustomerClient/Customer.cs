4108b2e710502951ae316c2cccdbb6a12a74c593
Fridai <Nikolaiviby3@gmail.com>
2018-03-23 10:01:54 +0100

    ll

---
+++ RestCustomerClient/Customer.cs
@@ -0,0 +1,33 @@
+﻿namespace RestCustomerClient
+{
+    public class Customer
+    {
+        private int _ID;
+        private string _FirstName;
+        private string _LastName;
+        private int _Year;
+
+        public Customer(int id, string fname, string lname, int year)
+        {
+            _ID = id;
+            _FirstName = fname;
+            _LastName = lname;
+            _Year = year;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        public int GetiD { get => _ID; set => _ID = value; }
+
+        public string GetFirstName { get => _FirstName; set => _FirstName = value; }
+
+        public string GetLastName { get => _LastName; set => _LastName = value; }
+
+        public int GetYear { get => _Year; set => _Year = value; }
+
+
+    }
+}
--- RestCustomerClient/Program.cs
+++ RestCustomerClient/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestCustomerClient
 {
     class Program
     {
+        Customer nnn = new Customer();
+
         static void Main(string[] args)
         {
+            Task<IList<Customer>> s = GetCustomersAsync();
+
+            IList<Customer> p = s.Result;
+            
+            foreach (var c in p)
+            {
+                Console.WriteLine($"navn: {c.GetFirstName} LastNavn: {c.GetLastName}  Født: {c.GetYear}  med ID: {c.GetiD}");
+            }
+
+            Console.ReadLine();
+
+        }
+        private static async Task<IList<Customer>> GetCustomersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync("http://localhost:7156/Service1.svc/customers/");
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"navn: {nnn.GetFirstName} LastNavn: {nnn.GetLastName}  Født: {nnn.GetYear}  med ID: {nnn.GetiD}";
         }
     }
 }
